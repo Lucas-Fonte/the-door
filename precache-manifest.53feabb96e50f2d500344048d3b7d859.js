c45850030c1d85533caef375390a0e17ee3bee94
Lucas Souza da Fonte <fontelucas@yahoo.com.br>
2020-05-22 18:31:21 -0300

    Updates

---
+++ precache-manifest.53feabb96e50f2d500344048d3b7d859.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359994d95ef0a59d25f76bc61f700de7",
+    "url": "/the-door/index.html"
+  },
+  {
+    "revision": "db0ed6384d7913fb8664",
+    "url": "/the-door/static/js/2.87eeff9e.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/the-door/static/js/2.87eeff9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "535161b27743b7717148",
+    "url": "/the-door/static/js/main.58e8bf6a.chunk.js"
+  },
+  {
+    "revision": "8f2da9ac63cb56ead1db",
+    "url": "/the-door/static/js/runtime-main.740d3adc.js"
+  }
+]);
